5cf339aaa9fa95cb5acef5a3fa08b01582030378
wangpkReal <1798852709@qq.com>
2023-04-29 00:31:47 +0800

    [Add]增加爬取蔬果网

--- constants.py
+++ constants.py
@@ -6,6 +6,7 @@ class TaskType(IntEnum):
     XIACHUFANG = 1  # 下厨房
     XINSHIPU = 2  # 心食谱
     MEISHITIANXIA = 3  # 美食天下
+    SHUGUOWANG = 4  # 蔬果网
 
 
 @unique
@@ -13,3 +14,4 @@ class TaskUrl(Enum):
     XIACHUFANG = 'https://www.xiachufang.com/explore/?page=1'
     XINSHIPU = 'https://www.xinshipu.com/jiachangzuofa/16485/'
     MEISHITIANXIA = 'https://home.meishichina.com/recipe/guangdongxiaochi/'
+    SHUGUOWANG = 'http://www.vegnet.com.cn/Market/477.html?page=1'
